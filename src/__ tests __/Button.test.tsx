2379ccf3b04c9e05e778a27a46e62817d787bb53
Tatsiana-prog <programvtv07@gmail.com>
2025-07-20 15:21:29 +0300

    feat: add test file for searsh

--- src/__ tests __/Button.test.tsx
+++ src/__ tests __/Button.test.tsx
@@ -5,18 +5,22 @@ import Button from '../components/Button/Button';
 
 test('renders button with correct text', () => {
   const onClickMock = jest.fn();
-  const { getByText } = render(<Button btnName="Search" onClick={onClickMock} />);
-  
+  const { getByText } = render(
+    <Button btnName="Search" onClick={onClickMock} />
+  );
+
   const buttonElement = getByText('Search');
   expect(buttonElement).toBeInTheDocument();
 });
 
 test('calls onClick handler when button is clicked', () => {
   const onClickMock = jest.fn();
-  const { getByText } = render(<Button btnName="Search" onClick={onClickMock} />);
-  
+  const { getByText } = render(
+    <Button btnName="Search" onClick={onClickMock} />
+  );
+
   const buttonElement = getByText('Search');
   fireEvent.click(buttonElement);
-  
+
   expect(onClickMock).toHaveBeenCalledTimes(1);
 });
